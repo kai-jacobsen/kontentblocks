76005263f09f570148c65555996f5d80ef7e3b0e
Kai Jacobsen <ungestaltbar@gmail.com>
2014-07-08 13:10:38 +0200

    minor array access fix

--- core/Fields/AbstractFieldManager.php
+++ core/Fields/AbstractFieldManager.php
@@ -5,137 +5,143 @@ namespace Kontentblocks\Fields;
  * Class AbstractFieldManager
  * @package Kontentblocks\Fields
  */
-abstract class AbstractFieldManager {
+abstract class AbstractFieldManager
+{
 
 
-	/**
-	 * Collection of added Sections / Fields ...
-	 * @var object
-	 * @since 1.0.0
-	 */
-	public $Structure;
+    /**
+     * Collection of added Sections / Fields ...
+     * @var object
+     * @since 1.0.0
+     */
+    public $Structure;
 
 
-	/**
-	 * Object to handle the section layout
-	 * e.g. defaults to tabs
-	 * @var object
-	 * @since 1.0.0
-	 */
-	protected $Render;
+    /**
+     * Object to handle the section layout
+     * e.g. defaults to tabs
+     * @var object
+     * @since 1.0.0
+     */
+    protected $Render;
 
 
-	/**
-	 * registered fields in one flat array
-	 * @var array
-	 * @since 1.0.0
-	 */
-	protected $fieldsById;
+    /**
+     * registered fields in one flat array
+     * @var array
+     * @since 1.0.0
+     */
+    protected $fieldsById;
 
 
-	/**
-	 * Prepare fields for frontend output
-	 *
-	 * @param array $instanceData
-	 *
-	 * @since 1.0.0
-	 */
-	public function setup( $instanceData ) {
+    /**
+     * Prepare fields for frontend output
+     *
+     * @param array $instanceData
+     *
+     * @since 1.0.0
+     */
+    public function setup( $instanceData )
+    {
 
-		if ( empty( $this->fieldsById ) ) {
-			$this->fieldsById = $this->collectAllFields();
-		}
-		foreach ( $this->fieldsById as $field ) {
-			$data = ( array_key_exists($field->getKey(), $instanceData ) ) ? $instanceData[ $field->getKey() ] : '';
-			$field->setup( $data, $this->baseId );
-		}
+        if (empty( $this->fieldsById )) {
+            $this->fieldsById = $this->collectAllFields();
+        }
+        foreach ($this->fieldsById as $field) {
+            $data = ( isset( $instanceData[$field->getKey()] ) ) ? $instanceData[$field->getKey()] : '';
+            $field->setup( $data, $this->baseId );
+        }
 
-	}
-
-
-	/**
-	 * Helper method to check whether an section already
-	 * exists in group
-	 *
-	 * @param string $id
-	 *
-	 * @return object
-	 * @since 1.0.0
-	 */
-	public function idExists( $id ) {
-		// TODO Test for right inheritance / abstract class
-		return ( isset( $this->Structure[ $id ] ) );
-
-	}
-
-	/**
-	 * Extract single fields from Structure object
-	 * and stores them in one single flat array
-	 * @return array
-	 * @since 1.0.0
-	 */
-	public function collectAllFields() {
-		$collect = array();
-		foreach ( $this->Structure as $def ) {
-			$collect = $collect + $def->getFields();
-		}
-
-		return $collect;
-
-	}
-
-	/**
-	 * Get a field object by key
-	 * returns the object on success
-	 * or false if key does not exist
-	 *
-	 * @param string $key
-	 *
-	 * @return mixed
-	 * @since 1.0.0
-	 */
-	public function getFieldByKey( $key, $fromArray = null ) {
-		if ( empty( $this->fieldsById ) ) {
-			$this->fieldsById = $this->collectAllFields();
-		}
-
-		if ( isset( $fromArray ) && $this->fieldsById[ $fromArray ] ) {
-			return $this->fieldsById[ $fromArray ]->getFieldByKey( $key );
-		}
+    }
 
 
-		if ( isset( $this->fieldsById[ $key ] ) ) {
-			return $this->fieldsById[ $key ];
-		} else {
-			false;
-		}
+    /**
+     * Helper method to check whether an section already
+     * exists in group
+     *
+     * @param string $id
+     *
+     * @return object
+     * @since 1.0.0
+     */
+    public function idExists( $id )
+    {
+        // TODO Test for right inheritance / abstract class
+        return ( isset( $this->Structure[$id] ) );
 
-	}
+    }
+
+    /**
+     * Extract single fields from Structure object
+     * and stores them in one single flat array
+     * @return array
+     * @since 1.0.0
+     */
+    public function collectAllFields()
+    {
+        $collect = array();
+        foreach ($this->Structure as $def) {
+            $collect = $collect + $def->getFields();
+        }
+
+        return $collect;
+
+    }
+
+    /**
+     * Get a field object by key
+     * returns the object on success
+     * or false if key does not exist
+     *
+     * @param string $key
+     *
+     * @return mixed
+     * @since 1.0.0
+     */
+    public function getFieldByKey( $key, $fromArray = null )
+    {
+        if (empty( $this->fieldsById )) {
+            $this->fieldsById = $this->collectAllFields();
+        }
+
+        if (isset( $fromArray ) && $this->fieldsById[$fromArray]) {
+            return $this->fieldsById[$fromArray]->getFieldByKey( $key );
+        }
 
 
-	/**
-	 * Calls save on each group
-	 *
-	 * @param $data
-	 * @param $oldData
-	 *
-	 * @return array
-	 * @since 1.0.0
-	 */
-	public function save( $data, $oldData ) {
-		$collection = array();
-		foreach ( $this->Structure as $definition ) {
+        if (isset( $this->fieldsById[$key] )) {
+            return $this->fieldsById[$key];
+        } else {
+            false;
+        }
 
-			$return     = ( $definition->save( $data, $oldData ) );
-			$collection = $collection + $return;
-		}
+    }
 
-		return $collection;
 
-	}
+    /**
+     * Calls save on each group
+     *
+     * @param $data
+     * @param $oldData
+     *
+     * @return array
+     * @since 1.0.0
+     */
+    public function save( $data, $oldData )
+    {
+        $collection = array();
+        foreach ($this->Structure as $definition) {
 
-	abstract public function renderFields();
+            $return     = ( $definition->save( $data, $oldData ) );
+            $collection = $collection + $return;
+        }
 
-	abstract public function addGroup( $id, $args = array() );
+        return $collection;
+
+    }
+
+    abstract public function renderFields();
+
+    abstract public function addGroup( $id, $args = array() );
 
 } 
