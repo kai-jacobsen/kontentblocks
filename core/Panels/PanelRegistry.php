5f99f741ddc8399ebacadf2d12d89a5e02609102
Kai Jacobsen <ungestaltbar@gmail.com>
2014-07-30 10:19:34 +0200

    formatting

--- core/Panels/PanelRegistry.php
+++ core/Panels/PanelRegistry.php
@@ -2,54 +2,60 @@
 
 namespace Kontentblocks\Panels;
 
-class PanelRegistry {
+class PanelRegistry
+{
 
-	public static $instance;
+    public static $instance;
 
-	/**
-	 * Panels collection
-	 * @var array
-	 */
-	public $panels = array();
+    /**
+     * Panels collection
+     * @var array
+     */
+    public $panels = array();
 
-	/**
-	 * @return PanelRegistry
-	 */
-	public static function getInstance() {
-		if ( null == self::$instance ) {
-			self::$instance = new self;
-		}
+    /**
+     * @return PanelRegistry
+     */
+    public static function getInstance()
+    {
+        if (null == self::$instance) {
+            self::$instance = new self;
+        }
 
-		return self::$instance;
-	}
+        return self::$instance;
+    }
 
-	/**
-	 * Add a Panel
-	 *
-	 * @param $id string
-	 * @param $args array
-	 *
-	 * @throws \Exception
-	 */
-	public function add( $id, $args ) {
-		if ( !isset( $this->panels[ $id ] ) ) {
-			if ( isset( $args['moduleClass'] ) && class_exists( $args['moduleClass'] ) ) {
-				$this->panels[ $id ] = new ModulePanel( $args );
-			} elseif ( isset( $args['formClass'] ) && class_exists( $args['formClass'] ) ) {
-				$this->panels[ $id ] = new $args['formClass']( $args );
-			} else {
-				throw new \Exception( 'No valid Class given' );
-			}
+    /**
+     * Add a Panel
+     *
+     * @param $id string
+     * @param $args array
+     *
+     * @throws \Exception
+     */
+    public function add( $id, $args )
+    {
+        if (!isset( $this->panels[$id] )) {
+            if (isset( $args['moduleClass'] ) && class_exists( $args['moduleClass'] )) {
+                $this->panels[$id] = new ModulePanel( $args );
+            } elseif (isset( $args['formClass'] ) && class_exists( $args['formClass'] )) {
+                $this->panels[$id] = new $args['formClass']( $args );
+            } else {
+                throw new \Exception( 'No valid Class given' );
+            }
 
-		} else {
-			throw new \Exception( 'Panel with same ID already exists' );
-		}
-	}
+        } else {
+            throw new \Exception(
+                'Error while adding panel to registry. Either a Panel with the same ID exist or the class does not exist'
+            );
+        }
+    }
 
-	public function get( $id ) {
-		if ( isset( $this->panels[ $id ] ) ) {
-			return $this->panels[ $id ];
-		}
-	}
+    public function get( $id )
+    {
+        if (isset( $this->panels[$id] )) {
+            return $this->panels[$id];
+        }
+    }
 
 }
--- core/Panels/StaticPanel.php
+++ core/Panels/StaticPanel.php
@@ -1,8 +1,6 @@
 <?php
 namespace Kontentblocks\Panels;
 
-
-use Kontentblocks\Fields\FieldManagerPanels;
 use Kontentblocks\Fields\PanelFieldManager;
 use Kontentblocks\Utils\Utilities;
 
@@ -10,249 +8,290 @@ use Kontentblocks\Utils\Utilities;
  * Class StaticPanel
  * @package Kontentblocks\Panels
  */
-abstract class StaticPanel {
+abstract class StaticPanel
+{
 
-	/**
-	 * Key / base id
-	 * @var string
-	 */
-	protected $baseId;
+    /**
+     * Key / base id
+     * @var string
+     */
+    protected $baseId;
 
-	/**
-	 * Post Types
-	 * @var array
-	 */
-	protected $postTypes = array();
+    /**
+     * Post Types
+     * @var array
+     */
+    protected $postTypes = array();
 
-	/**
-	 * PageTemplates
-	 * @var array
-	 */
-	protected $pageTemplates = array();
+    /**
+     * PageTemplates
+     * @var array
+     */
+    protected $pageTemplates = array();
 
-	/**
-	 * Position / Hook to use
-	 * @var string
-	 */
-	protected $hook;
+    /**
+     * Position / Hook to use
+     * @var string
+     */
+    protected $hook;
 
-	/**
-	 * Render in MetaBox
-	 * @var bool
-	 */
-	protected $metaBox;
+    /**
+     * Render in MetaBox
+     * @var bool
+     */
+    protected $metaBox;
 
-	/**
-	 * Custom Field Manager Instance
-	 * @var PanelFieldManager
-	 */
-	protected $FieldManager;
+    /**
+     * Custom Field Manager Instance
+     * @var PanelFieldManager
+     */
+    protected $FieldManager;
 
-	/**
-	 * Flag indicates if data should be stored as single key => value pairs
-	 * in the meta table
-	 * @var bool
-	 */
-	protected $saveAsSingle = false;
+    /**
+     * Flag indicates if data should be stored as single key => value pairs
+     * in the meta table
+     * @var bool
+     */
+    protected $saveAsSingle = false;
 
-	/**
-	 * Form data
-	 * @var array
-	 */
-	protected $data = null;
+    /**
+     * Form data
+     * @var array
+     */
+    protected $data = null;
 
-	/**
-	 * Class constructor
-	 *
-	 * @param array $args
-	 *
-	 * @throws \Exception
-	 */
-	public function __construct( $args ) {
-		$args = $this->parseDefaults( $args );
+    /**
+     * Class constructor
+     *
+     * @param array $args
+     *
+     * @throws \Exception
+     */
+    public function __construct( $args )
+    {
+        $args = $this->parseDefaults( $args );
 
-		if ( is_null( $args['baseId'] ) ) {
-			throw new \Exception( 'MUST provide a base id' );
-		}
+        if (is_null( $args['baseId'] )) {
+            throw new \Exception( 'MUST provide a base id' );
+        }
 
-		// mumbo jumbo
-		$this->setupArgs( $args );
-		$this->setupHooks();
-	}
+        // mumbo jumbo
+        $this->setupArgs( $args );
+        $this->setupHooks();
+    }
 
-	private function parseDefaults( $args ) {
-		$defaults = array(
-			'baseId'        => null,
-			'metaBox'       => false,
-			'hook'          => 'edit_form_after_title',
-			'postTypes'     => array(),
-			'pageTemplates' => array( 'default' )
-		);
+    /**
+     * Extend arg with defaults
+     * @param $args
+     * @return array
+     */
+    private function parseDefaults( $args )
+    {
+        $defaults = array(
+            'baseId' => null,
+            'metaBox' => false,
+            'hook' => 'edit_form_after_title',
+            'postTypes' => array(),
+            'pageTemplates' => array( 'default' )
+        );
 
-		return wp_parse_args( $args, $defaults );
-	}
+        return wp_parse_args( $args, $defaults );
+    }
 
-	abstract public function fields( PanelFieldManager $FieldManager );
+    /**
+     * Fields to render, must be provided by child class
+     * @param PanelFieldManager $FieldManager
+     * @return mixed
+     */
+    abstract public function fields( PanelFieldManager $FieldManager );
 
 
-	private function setupArgs( $args ) {
-		foreach ( $args as $k => $v ) {
-			if ( method_exists( $this, "set" . strtoupper( $k ) ) ) {
-				$method = "set" . strtoupper( $k );
-				$this->$method( $v );
-			} else {
-				$this->$k = $v;
-			}
-		}
-	}
+    /**
+     * Auto setup args to class properties
+     * and look for optional method for each arg
+     * @param $args
+     */
+    private function setupArgs( $args )
+    {
+        foreach ($args as $k => $v) {
+            if (method_exists( $this, "set" . strtoupper( $k ) )) {
+                $method = "set" . strtoupper( $k );
+                $this->$method( $v );
+            } else {
+                $this->$k = $v;
+            }
+        }
+    }
 
-	private function setupHooks() {
-		foreach ( $this->postTypes as $pt ) {
+    /**
+     * Setup wordpress hooks
+     */
+    private function setupHooks()
+    {
+        foreach ($this->postTypes as $pt) {
 
-			if ( $pt === 'page' && !empty( $this->pageTemplates ) ) {
-				$tpl = get_post_meta( '_wp_page_template' );
-				if ( empty( $tpl ) || !in_array( $tpl, $this->pageTemplates ) ) {
-					continue;
-				}
-			}
+            // check for page templates resp. for the a _wp_page_template meta key
+            if (!empty( $this->pageTemplates )) {
+                $tpl = get_post_meta( '_wp_page_template' );
+                if (empty( $tpl ) || !in_array( $tpl, $this->pageTemplates )) {
+                    continue;
+                }
+            }
 
-			if ( $this->metaBox ) {
-				add_action( "add_meta_boxes_{$pt}", array( $this, 'metaBox' ), 10, 1 );
-			} else {
-				add_action( $this->hook, array( $this, 'form' ) );
-			}
-			add_action( "save_post", array( $this, 'save' ), 10, 1 );
-		}
-	}
+            // either add the form as meta box or to custom hook
+            if (is_array( $this->metaBox )) {
+                add_action( "add_meta_boxes_{$pt}", array( $this, 'metaBox' ), 10, 1 );
+            } else {
+                add_action( $this->hook, array( $this, 'form' ) );
+            }
+            add_action( "save_post", array( $this, 'save' ), 10, 1 );
+        }
+    }
 
-	public function metaBox( $postObj ) {
+    /**
+     * add meta box action callback
+     * @param $postObj
+     */
+    public function metaBox( $postObj )
+    {
 
-		if ( !post_type_supports( $postObj->post_type, 'editor' ) ) {
+        if (!post_type_supports( $postObj->post_type, 'editor' )) {
             Utilities::hiddenEditor();
-		}
+        }
 
 
-		$defaults = array(
-			'title'        => 'No Title provided',
-			'context'      => 'advanced',
-			'priority'     => 'high',
-			'saveAsSingle' => false
-		);
+        $defaults = array(
+            'title' => 'No Title provided',
+            'context' => 'advanced',
+            'priority' => 'high',
+            'saveAsSingle' => false
+        );
 
-		$mb = wp_parse_args( $this->metaBox, $defaults );
+        $mb = wp_parse_args( $this->metaBox, $defaults );
 
-		if ( $this->metaBox ) {
-			add_meta_box( $this->baseId,
-				$mb['title'],
-				array( $this, 'form' ),
-				$postObj->post_type,
-				$mb['context'],
-				$mb['priority'] );
-		}
-	}
+        if ($this->metaBox) {
+            add_meta_box(
+                $this->baseId,
+                $mb['title'],
+                array( $this, 'form' ),
+                $postObj->post_type,
+                $mb['context'],
+                $mb['priority']
+            );
+        }
+    }
 
-	public function form( $postObj ) {
+    /**
+     * Render fields
+     * @param $postObj
+     */
+    public function form( $postObj )
+    {
 
-		if ( !in_array( $postObj->post_type, $this->postTypes ) ) {
-			return;
-		}
+        if (!in_array( $postObj->post_type, $this->postTypes )) {
+            return;
+        }
 
-		if ( !post_type_supports( $postObj->post_type, 'editor' ) ) {
+        if (!post_type_supports( $postObj->post_type, 'editor' )) {
             Utilities::hiddenEditor();
-		}
+        }
 
-		$this->setupData( $postObj->ID );
-		$this->FieldManager = new PanelFieldManager( $this->baseId, $this->data, $this );
+        $this->setupData( $postObj->ID );
+        $this->FieldManager = new PanelFieldManager( $this->baseId, $this->data, $this );
 
-		$this->beforeForm();
-		$this->fields( $this->FieldManager )->renderFields();
-		$this->afterForm();
-	}
+        $this->beforeForm();
+        $this->fields( $this->FieldManager )->renderFields();
+        $this->afterForm();
+    }
 
-	// @TODO simplyfy field manager instantiation
-	public function save( $postId ) {
+    /**
+     * @param $postId
+     */
+    public function save( $postId )
+    {
 
-		if ( empty( $_POST[ $this->baseId ] ) ) {
-			return;
-		}
+        if (empty( $_POST[$this->baseId] )) {
+            return;
+        }
 
-		$old                = $this->setupData( $postId );
-		$this->FieldManager = new PanelFieldManager( $this->baseId, $this->data, $this );
+        $old = $this->setupData( $postId );
+        $this->FieldManager = new PanelFieldManager( $this->baseId, $this->data, $this );
 
-		$new = $this->fields( $this->FieldManager )->save( $_POST[ $this->baseId ], $old );
-		update_post_meta( $postId, $this->baseId, $new );
+        $new = $this->fields( $this->FieldManager )->save( $_POST[$this->baseId], $old );
+        update_post_meta( $postId, $this->baseId, $new );
 
-		if ( $this->saveAsSingle ) {
-			foreach ( $new as $k => $v ) {
-				if ( empty( $v ) ) {
-					delete_post_meta( $postId, $this->baseId . '_' . $k );
-				} else {
-					update_post_meta( $postId, $this->baseId . '_' . $k, $v );
-				}
-			}
-		}
-	}
+        if ($this->saveAsSingle) {
+            foreach ($new as $k => $v) {
+                if (empty( $v )) {
+                    delete_post_meta( $postId, $this->baseId . '_' . $k );
+                } else {
+                    update_post_meta( $postId, $this->baseId . '_' . $k, $v );
+                }
+            }
+        }
+    }
 
-	/**
-	 * Markup before inner form
-	 */
-	private function beforeForm() {
-		echo "<div class='postbox'>
+    /**
+     * Markup before inner form
+     */
+    private function beforeForm()
+    {
+        echo "<div class='postbox'>
                 <div class='kb-custom-wrapper'>
                 <div class='handlediv' title='Zum Umschalten klicken'></div><div class='inside'>";
-	}
+    }
 
-	/**
-	 * Markup after
-	 */
-	private function afterForm() {
-		echo "</div></div></div>";
-	}
+    /**
+     * Markup after
+     */
+    private function afterForm()
+    {
+        echo "</div></div></div>";
+    }
 
-	/**
-	 * Setup panel related meta data
-	 *
-	 * @param $postId
-	 *
-	 * @TODO use meta api?
-	 */
-	private function setupData( $postId ) {
-		if ( is_object( $postId ) ) {
-			$id = $postId->ID;
-		} else {
-			$id = $postId;
-		}
+    /**
+     * Setup panel related meta data
+     * @param $postId
+     * @return array
+     */
+    private function setupData( $postId )
+    {
+        if (is_object( $postId )) {
+            $id = $postId->ID;
+        } else {
+            $id = $postId;
+        }
 
+        $this->data = get_post_meta( $id, $this->baseId, true );
+        return $this->data;
+    }
 
-		$this->data = get_post_meta( $id, $this->baseId, true );
-
-		return $this->data;
-	}
-
-	/**
-	 * Manually set up fielddata
-	 * Makes it possible to get the Panel from the registry, and use it as data container
-	 * @TODO __Revise__
-	 *
-	 * @param $postId
-	 *
-	 * @return $this
-	 */
-	public function setup( $postId ) {
-		$this->setupData( $postId );
-		$this->FieldManager = new PanelFieldManager( $this->baseId, $this->data, $this );
+    /**
+     * Manually set up fielddata
+     * Makes it possible to get the Panel from the registry, and use it as data container
+     * @TODO __Revise__
+     *
+     * @param $postId
+     *
+     * @return $this
+     */
+    public function setup( $postId )
+    {
+        $this->setupData( $postId );
+        $this->FieldManager = new PanelFieldManager( $this->baseId, $this->data, $this );
         $this->fields( $this->FieldManager )->setup( $this->data );
 
         return $this;
 
-	}
+    }
 
-	/**
-	 * After setup, get the setup object
-	 * @return array
-	 * @TODO __Revise__
-	 */
-	public function getData() {
-		return $this->FieldManager->prepareDataAndGet();
-	}
+    /**
+     * After setup, get the setup object
+     * @return array
+     * @TODO __Revise__
+     */
+    public function getData()
+    {
+        return $this->FieldManager->prepareDataAndGet();
+    }
 }
