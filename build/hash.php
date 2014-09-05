5d7d26c77a00e1c97a3b672caa742dc3bdcd961c
Kai Jacobsen <ungestaltbar@gmail.com>
2014-09-05 09:58:33 +0200

    namespaced handlebars

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "54096c4a5fca8";
+	return "29e5a579ef3ab3bae8f2cd69daab80234e1c8e81";
 };
