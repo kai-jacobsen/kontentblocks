d002b9c77ead4bcd4144ebb1572aa2e7d2b5c6d2
Kai Jacobsen <ungestaltbar@gmail.com>
2014-11-23 20:10:52 +0100

    merged master

--- build/hash.php
+++ build/hash.php
@@ -1,7 +1,7 @@
 <?php
 // This file gets automatically generated when grunt rebuilds js/css
 // either returns the current git hash or a random string
-// this will make sure that the localStorage gets invalidated upon changes
+// this will invalidate the local storage upon changes
 function getGitHash(){
-	return "54721f49513ca";
+	return "1256e3506c2606d53f263be04eb2fef879496392";
 };
