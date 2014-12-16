b5d8dae5340f348ae2f1e7ae87841b1d7be26325
Kai Jacobsen <ungestaltbar@gmail.com>
2014-12-16 14:54:19 +0100

    automatic master rebuild

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "dee8a1b3bdd679f33e55dcd544d01963bda11223";
+	return "e9f14827102bfcc4e41afded976d669f77f34822";
 };
