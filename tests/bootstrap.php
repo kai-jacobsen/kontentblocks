0ca71ea74f52051351f3eae331b837e1bda36636
Kai Jacobsen <ungestaltbar@gmail.com>
2014-12-17 15:30:18 +0100

    updated travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
+$_tests_dir = getenv( 'WP_DEVELOP_DIR' );
 if (!$_tests_dir) {
     $_tests_dir = '/tmp/wordpress/wordpress-tests-lib';
 }
 
-require_once $_tests_dir . '/functions.php';
+require_once $_tests_dir . '/tests/phpunit/includes/functions.php';
 
 function _manually_load_plugin()
 {
