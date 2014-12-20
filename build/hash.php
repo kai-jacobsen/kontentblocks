d4dde5175ad049a41ca3a034d293adf7b519e9f3
Kai Jacobsen <ungestaltbar@gmail.com>
2014-12-20 23:04:57 +0100

    included grunt step in travis

--- build/hash.php
+++ build/hash.php
@@ -1,7 +1,7 @@
 <?php
 // This file gets automatically generated when grunt rebuilds js/css
 // either returns the current git hash or a random string
-// this will make sure that the localStorage gets invalidated upon changes
+// this will invalidate the local storage upon changes
 function getGitHash(){
-	return "5495d337b51bf";
+	return "f20ac40088256d3ddba910bb1a04a900f11cac21";
 };
