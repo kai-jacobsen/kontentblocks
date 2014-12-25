0f541b496eeb50f168557ebb637d0a5d4eb9278d
Kai Jacobsen <ungestaltbar@gmail.com>
2014-12-25 15:21:42 +0100

    rebuilt

---
+++ build/hash.php
@@ -0,0 +1,7 @@
+<?php
+// This file gets automatically generated when grunt rebuilds js/css
+// either returns the current git hash or a random string
+// this will invalidate the local storage upon changes
+function getGitHash(){
+	return "da277dea1350a4a445801c6b3125b26fc28acca6";
+};
