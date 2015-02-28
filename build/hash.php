78b091d367c5f30d1e95efb29e489fb1a8fac1bb
Kai Jacobsen <ungestaltbar@gmail.com>
2015-02-28 21:13:19 +0100

    rebuilt

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "347020ff5d5862360fd9f62f54c9c1ecccc8f584";
+	return "ebca52d53101084cc91cc179f67f759d0c83fa0b";
 };
