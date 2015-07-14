fe8e4fc703cfaf6ea27ef25e46b01b845bfe1cec
Kai Jacobsen <ungestaltbar@gmail.com>
2015-07-14 17:00:11 +0200

    css fixes

--- build/hash.php
+++ build/hash.php
@@ -1,7 +1,7 @@
 <?php
 // This file gets automatically generated when grunt rebuilds js/css
 // either returns the current git hash or a random string
-// this will make sure that the localStorage gets invalidated upon changes
+// this will invalidate the local storage upon changes
 function getGitHash(){
-	return "55a516999aad1";
+	return "6d1c650003a0b4009a31ca6cc1253e92f8cd04f3";
 };
