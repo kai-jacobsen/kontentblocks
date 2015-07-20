dd34973483875667e18277a8a5afea918d7fc1cd
Kai Jacobsen <ungestaltbar@gmail.com>
2015-07-20 11:28:28 +0200

    panel view dirty method

--- js/src/backend/Views/PanelView.js
+++ js/src/backend/Views/PanelView.js
@@ -1,6 +1,14 @@
+var Logger = require('common/Logger');
 KB.Backbone.Backend.PanelView = Backbone.View.extend({
 
   initialize: function(){
     this.model.View = this;
+  },
+  getDirty: function(){
+    Logger.info('Panel data dirty');
+  },
+  getClean: function(){
+    Logger.info('Panel data clean');
   }
+
 });
