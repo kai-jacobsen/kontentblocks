e7073d494785538018f0a5026828f4543c6e5cb1
Kai Jacobsen <ungestaltbar@gmail.com>
2015-07-22 23:51:02 +0200

    panels

--- tests/core/Backend/Screen/EditScreenTest.php
+++ tests/core/Backend/Screen/EditScreenTest.php
@@ -56,7 +56,7 @@ class EditScreenTest extends \WP_UnitTestCase
     {
         $post = $this->factory->post->create_and_get();
         add_post_type_support( 'post', 'kontentblocks' );
-        $out = $this->EditScreen->userInterface( $post );
+        $out = $this->EditScreen->userInterface( $post->post_type,$post );
         $this->assertContains( "id='kontentblocks-core-ui'", $out );
         $this->assertContains( "</div> <!--end ks -->", $out );
         $this->assertContains( "id='demo-content'", $out );
