08147550c7e18005c102cf69975c1465ded92406
Kai Jacobsen <ungestaltbar@gmail.com>
2015-08-18 17:16:08 +0200

    customizer support for field type link | option panels

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "189345f185c30884c2652a2ead13af2680f4eefe";
+	return "b3e9c02228120eb0e099dc078784ceb6271458d5";
 };
---
+++ js/dev/customizer.js
@@ -0,0 +1,117 @@
+/*! Kontentblocks DevVersion 2015-08-18 */
+(function e(t, n, r) {
+    function s(o, u) {
+        if (!n[o]) {
+            if (!t[o]) {
+                var a = typeof require == "function" && require;
+                if (!u && a) return a(o, !0);
+                if (i) return i(o, !0);
+                var f = new Error("Cannot find module '" + o + "'");
+                throw f.code = "MODULE_NOT_FOUND", f;
+            }
+            var l = n[o] = {
+                exports: {}
+            };
+            t[o][0].call(l.exports, function(e) {
+                var n = t[o][1][e];
+                return s(n ? n : e);
+            }, l, l.exports, e, t, n, r);
+        }
+        return n[o].exports;
+    }
+    var i = typeof require == "function" && require;
+    for (var o = 0; o < r.length; o++) s(r[o]);
+    return s;
+})({
+    1: [ function(require, module, exports) {
+        wp.customize.controlConstructor.kbLink = require("customizer/controls/Link");
+    }, {
+        "customizer/controls/Link": 2
+    } ],
+    2: [ function(require, module, exports) {
+        var Link = require("fields/controls/link");
+        module.exports = wp.customize.Control.extend({
+            ready: function() {
+                var control = this;
+                _.bind(control.updateValue, control);
+                control.LinkField = new Link({
+                    el: control.selector
+                });
+                jQuery("#wp-link-wrap").css("zIndex", "99999999");
+                var fn = _.bind(_.debounce(control.updateValue, 400), control);
+                control.LinkField.on("update", fn, 400);
+                control.LinkField.$text.on("keyup", fn);
+                control.LinkField.$input.on("keyup", fn);
+            },
+            updateValue: function() {
+                var control = this;
+                var value = {
+                    linktext: control.LinkField.$text.val(),
+                    link: control.LinkField.$input.val()
+                };
+                control.setting.set(value);
+            }
+        });
+    }, {
+        "fields/controls/link": 4
+    } ],
+    3: [ function(require, module, exports) {
+        module.exports = Backbone.View.extend({
+            rerender: function() {
+                this.render();
+            }
+        });
+    }, {} ],
+    4: [ function(require, module, exports) {
+        var BaseView = require("../FieldBaseView");
+        module.exports = BaseView.extend({
+            initialize: function() {
+                window._kbLink = this;
+                this.render();
+            },
+            events: {
+                "click .kb-js-add-link": "openModal"
+            },
+            render: function() {
+                this.$input = this.$("[data-kbf-link-url]");
+                this.$text = this.$("[data-kbf-link-linktext]");
+            },
+            derender: function() {},
+            openModal: function() {
+                wpActiveEditor = this.$input.attr("id");
+                jQuery("#wp-link-wrap").addClass("kb-customized");
+                window.kb_restore_htmlUpdate = wpLink.htmlUpdate;
+                window.kb_restore_isMce = wpLink.isMCE;
+                wpLink.isMCE = this.isMCE;
+                wpLink.htmlUpdate = this.htmlUpdate;
+                wpLink.open();
+                jQuery("#wp-link-text").val(this.$text.val());
+                jQuery("#wp-link-url").val(this.$input.val());
+            },
+            htmlUpdate: function() {
+                var attrs, html, start, end, cursor, href, title, textarea = wpLink.textarea, result;
+                if (!textarea) return;
+                attrs = wpLink.getAttrs();
+                title = jQuery("#wp-link-text").val();
+                if (!attrs.href || attrs.href == "http://") return;
+                href = attrs.href;
+                jQuery(textarea).empty();
+                textarea.value = href;
+                window._kbLink.trigger("update", title, href);
+                window._kbLink.$text.val(title);
+                wpLink.close();
+                window._kbLink.close();
+                textarea.focus();
+            },
+            isMCE: function() {
+                return false;
+            },
+            close: function() {
+                wpLink.isMCE = window.kb_restore_isMce;
+                wpLink.htmlUpdate = window.kb_restore_htmlUpdate;
+            }
+        });
+    }, {
+        "../FieldBaseView": 3
+    } ]
+}, {}, [ 1 ]);
