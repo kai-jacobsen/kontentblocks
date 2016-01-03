962eeebc0972ee18c9956be5f051c3152d55df3d
Kai Jacobsen <ungestaltbar@gmail.com>
2016-01-03 16:21:41 +0100

    rebuilt after merge

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "dfed6a8bc190881e1f1078684be747078e7f90eb";
+	return "44dbb85db4e06679fa5bcd77786fc930f6df14a4";
 };
