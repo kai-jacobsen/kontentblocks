dc16389e3241cb91a5a39afa0335f3722332aed0
Kai Jacobsen <ungestaltbar@gmail.com>
2016-01-05 15:50:20 +0100

    flexfields bug if multiple editor instance in one item

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "0e57baa307e393acdb55b7fd0e6052a6e2bbce80";
+	return "b6a8f81d1f304b2df0c4814a3d9c25428113c217";
 };
