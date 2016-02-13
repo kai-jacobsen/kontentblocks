ddf787f6f1b9a80480b99494bc3bfad8e0ce0ecf
Kai Jacobsen <ungestaltbar@gmail.com>
2016-02-13 22:13:26 +0100

    api update

--- includes/kb-api.php
+++ includes/kb-api.php
@@ -14,11 +14,11 @@ use Kontentblocks\Utils\Utilities;
  * Register Area
  * @param $args
  */
-function registerArea( $args )
+function registerArea($args)
 {
     /** @var \Kontentblocks\Areas\AreaRegistry $AreaRegistry */
-    $AreaRegistry = Kontentblocks::getService( 'registry.areas' );
-    $AreaRegistry->addArea( $args, true );
+    $AreaRegistry = Kontentblocks::getService('registry.areas');
+    $AreaRegistry->addArea($args, true);
 
 }
 
@@ -26,7 +26,7 @@ function registerArea( $args )
  * Register Area template
  * @param $args
  */
-function registerAreaTemplate( $args )
+function registerAreaTemplate($args)
 {
     $defaults = array
     (
@@ -38,10 +38,10 @@ function registerAreaTemplate( $args )
         'cycle' => false
     );
 
-    $settings = wp_parse_args( $args, $defaults );
+    $settings = wp_parse_args($args, $defaults);
 
-    if (!empty( $settings['id'] )) {
-        Kontentblocks::getService( 'registry.areas' )->addTemplate( $settings );
+    if (!empty($settings['id'])) {
+        Kontentblocks::getService('registry.areas')->addTemplate($settings);
     }
 
 }
@@ -58,43 +58,43 @@ function registerAreaTemplate( $args )
  *
  * @return string|void
  */
-function renderSingleArea( $areaId, $post_id = null, $areaSettings = array(), $moduleSettings = array() )
+function renderSingleArea($areaId, $post_id = null, $areaSettings = array(), $moduleSettings = array())
 {
     global $post;
-    $postId = ( is_null( $post_id ) && !is_null( $post ) ) ? $post->ID : $post_id;
+    $postId = (is_null($post_id) && !is_null($post)) ? $post->ID : $post_id;
 
 
-    if (is_null( $postId )) {
+    if (is_null($postId)) {
         return null;
     }
 
 
     /** @var \Kontentblocks\Areas\AreaRegistry $registry */
-    $registry = Kontentblocks::getService( 'registry.areas' );
-    if ($registry->isDynamic( $areaId )) {
-        $areaDef = $registry->getArea( $areaId );
+    $registry = Kontentblocks::getService('registry.areas');
+    if ($registry->isDynamic($areaId)) {
+        $areaDef = $registry->getArea($areaId);
         $parentObjectId = apply_filters('kb.get.area.dynamic.id', $areaDef->parentObjectId);
-        $environment = Utilities::getPostEnvironment( $parentObjectId, $postId );
+        $environment = Utilities::getPostEnvironment($parentObjectId, $postId);
     } else {
-        $environment = Utilities::getPostEnvironment( $postId );
+        $environment = Utilities::getPostEnvironment($postId);
     }
 
-    $area = $environment->getAreaDefinition( $areaId );
+    $area = $environment->getAreaDefinition($areaId);
 
 
     if (!$area) {
         return '';
     }
 
-    $areaRenderSettings = new AreaRenderSettings( $areaSettings, $area );
-    $moduleRenderSettings = new ModuleRenderSettings( $moduleSettings );
-    if (is_a( $areaRenderSettings->view, '\Kontentblocks\Frontend\Renderer\AreaFileRenderer', true )) {
-        $renderer = new $areaRenderSettings->view( $environment, $areaRenderSettings, $moduleRenderSettings );
+    $areaRenderSettings = new AreaRenderSettings($areaSettings, $area);
+    $moduleRenderSettings = new ModuleRenderSettings($moduleSettings);
+    if (is_a($areaRenderSettings->view, '\Kontentblocks\Frontend\Renderer\AreaFileRenderer', true)) {
+        $renderer = new $areaRenderSettings->view($environment, $areaRenderSettings, $moduleRenderSettings);
     } else {
-        $renderer = new AreaRenderer( $environment, $areaRenderSettings, $moduleRenderSettings );
+        $renderer = new AreaRenderer($environment, $areaRenderSettings, $moduleRenderSettings);
     }
 
-    $renderer->render( true );
+    $renderer->render(true);
 }
 
 /**
@@ -103,15 +103,15 @@ function renderSingleArea( $areaId, $post_id = null, $areaSettings = array(), $m
  * @param array $areaSettings
  * @param array $moduleSettings
  */
-function renderSideAreas( $id, $areaSettings = array(), $moduleSettings = array() )
+function renderSideAreas($id, $areaSettings = array(), $moduleSettings = array())
 {
     global $post;
 
-    $post_id = ( null === $id ) ? $post->ID : $id;
-    $areas = get_post_meta( $post_id, 'active_sidebar_areas', true );
-    if (!empty( $areas )) {
+    $post_id = (null === $id) ? $post->ID : $id;
+    $areas = get_post_meta($post_id, 'active_sidebar_areas', true);
+    if (!empty($areas)) {
         foreach ($areas as $area) {
-            renderSingleArea( $area, $post_id, $areaSettings, $moduleSettings );
+            renderSingleArea($area, $post_id, $areaSettings, $moduleSettings);
         }
     }
 }
@@ -123,22 +123,22 @@ function renderSideAreas( $id, $areaSettings = array(), $moduleSettings = array(
  * @param array $areaSettings
  * @param array $moduleSettings
  */
-function renderContext( $context, $post_id, $areaSettings = array(), $moduleSettings = array() )
+function renderContext($context, $post_id, $areaSettings = array(), $moduleSettings = array())
 {
     global $post;
-    $postId = ( null === $post_id ) ? $post->ID : $post_id;
+    $postId = (null === $post_id) ? $post->ID : $post_id;
 
-    $Environment = Utilities::getPostEnvironment( $postId );
-    $areas = $Environment->getAreasForContext( $context );
-    $contextsOrder = $Environment->getDataProvider()->get( 'kb.contexts' );
+    $Environment = Utilities::getPostEnvironment($postId);
+    $areas = $Environment->getAreasForContext($context);
+    $contextsOrder = $Environment->getDataProvider()->get('kb.contexts');
 
-    if (is_array( $contextsOrder ) && !empty( $contextsOrder )) {
+    if (is_array($contextsOrder) && !empty($contextsOrder)) {
         foreach ($contextsOrder as $context => $areaIds) {
-            if (is_array( $areaIds )) {
-                foreach (array_keys( $areaIds ) as $areaId) {
-                    if (isset( $areas[$areaId] )) {
+            if (is_array($areaIds)) {
+                foreach (array_keys($areaIds) as $areaId) {
+                    if (isset($areas[$areaId])) {
                         $tmp = $areas[$areaId];
-                        unset( $areas[$areaId] );
+                        unset($areas[$areaId]);
                         $areas[$areaId] = $tmp;
                     }
                 }
@@ -146,21 +146,21 @@ function renderContext( $context, $post_id, $areaSettings = array(), $moduleSett
         }
     }
 
-    if (!empty( $areas )) {
-        foreach (array_keys( $areas ) as $area) {
-            if (array_key_exists( $area, $areaSettings )) {
-                $args = Utilities::arrayMergeRecursive( $areaSettings[$area], $areaSettings );
+    if (!empty($areas)) {
+        foreach (array_keys($areas) as $area) {
+            if (array_key_exists($area, $areaSettings)) {
+                $args = Utilities::arrayMergeRecursive($areaSettings[$area], $areaSettings);
             } else {
                 $args = $areaSettings;
             }
 
-            if (array_key_exists( $area, $moduleSettings )) {
-                $margs = Utilities::arrayMergeRecursive( $moduleSettings[$area], $moduleSettings );
+            if (array_key_exists($area, $moduleSettings)) {
+                $margs = Utilities::arrayMergeRecursive($moduleSettings[$area], $moduleSettings);
             } else {
                 $margs = $areaSettings;
             }
 
-            renderSingleArea( $area, $postId, $args, $margs );
+            renderSingleArea($area, $postId, $args, $margs);
         }
     }
 
@@ -173,17 +173,17 @@ function renderContext( $context, $post_id, $areaSettings = array(), $moduleSett
  * @param int $postId
  * @return mixed
  */
-function hasModules( $area, $postId )
+function hasModules($area, $postId)
 {
     global $post;
     if ($post === null && $postId === null) {
         return false;
     }
 
-    $Environment = Utilities::getPostEnvironment( $postId );
-    $areas = $Environment->getModulesForArea( $area );
+    $Environment = Utilities::getPostEnvironment($postId);
+    $areas = $Environment->getModulesForArea($area);
 
-    return !empty( $areas );
+    return !empty($areas);
 }
 
 
@@ -192,28 +192,33 @@ function hasModules( $area, $postId )
  * @param null $post_id
  * @return Panels\OptionsPanel|null|\WP_Error
  */
-function getPanel( $id = null, $post_id = null )
+function getPanel($id = null, $post_id = null)
 {
-    return getPostPanel( $id, $post_id );
+    return getPostPanel($id, $post_id);
 }
 
-function getPostPanel( $panelId = null, $postId = null )
+/**
+ * @param null $panelId
+ * @param null $postId
+ * @return Panels\PostPanel|null|\WP_Error
+ */
+function getPostPanel($panelId = null, $postId = null)
 {
 
-    if (is_null( $postId )) {
+    if (is_null($postId)) {
         $postId = get_the_ID();
     }
-    $Environment = Utilities::getPostEnvironment( $postId );
+    $Environment = Utilities::getPostEnvironment($postId);
 
-    $Panel = $Environment->getPanelObject( $panelId );
+    $Panel = $Environment->getPanelObject($panelId);
     /** @var \Kontentblocks\Panels\PostPanel $Panel */
-    if (is_a( $Panel, "\\Kontentblocks\\Panels\\AbstractPanel" )) {
+    if (is_a($Panel, "\\Kontentblocks\\Panels\\AbstractPanel")) {
         return $Panel;
     } else {
         return new \WP_Error(
             'Kontentblocks',
             'Panel with requested id does not exist.',
-            array( 'request' => $panelId, 'line' => __LINE__, 'file' => __FILE__ )
+            array('request' => $panelId, 'line' => __LINE__, 'file' => __FILE__)
         );
     }
 }
@@ -224,14 +229,14 @@ function getPostPanel( $panelId = null, $postId = null )
  * @param null $taxonomy
  * @return null|\WP_Error
  */
-function getTermPanel( $panelId, $termId, $taxonomy = null )
+function getTermPanel($panelId, $termId, $taxonomy = null)
 {
-    $environment = Utilities::getTermEnvironment( $termId, $taxonomy );
-    $panel = $environment->getTermPanel( $panelId );
-    if (is_a( $panel, "\\Kontentblocks\\Panels\\TermPanel" )) {
+    $environment = Utilities::getTermEnvironment($termId, $taxonomy);
+    $panel = $environment->getTermPanel($panelId);
+    if (is_a($panel, "\\Kontentblocks\\Panels\\TermPanel")) {
         return $panel;
     } else {
-        return new \WP_Error( 'Kontentblocks', 'Panel does not exist', array( 'request' => $panelId ) );
+        return new \WP_Error('Kontentblocks', 'Panel does not exist', array('request' => $panelId));
     }
 }
 
@@ -241,10 +246,10 @@ function getTermPanel( $panelId, $termId, $taxonomy = null )
  * @param null $taxonomy
  * @return mixed
  */
-function getTermPanelModel( $panelId, $termId, $taxonomy = null )
+function getTermPanelModel($panelId, $termId, $taxonomy = null)
 {
-    $panel = getTermPanel( $panelId, $termId, $taxonomy );
-    if (!is_wp_error( $panel )) {
+    $panel = getTermPanel($panelId, $termId, $taxonomy);
+    if (!is_wp_error($panel)) {
         return $panel->setupFrontendData();
     }
 }
@@ -254,20 +259,20 @@ function getTermPanelModel( $panelId, $termId, $taxonomy = null )
  * @param null $postId
  * @return mixed
  */
-function getPostPanelModel( $panelId = null, $postId = null )
+function getPostPanelModel($panelId = null, $postId = null)
 {
-    $panel = getPostPanel( $panelId, $postId );
-    if (is_a( $panel, '\Kontentblocks\Panels\PostPanel' )) {
+    $panel = getPostPanel($panelId, $postId);
+    if (is_a($panel, '\Kontentblocks\Panels\PostPanel')) {
         return $panel->setupFrontendData();
     }
     return null;
 }
 
-function getPostPanelView( $tpl = null, $panelId = null, $postId = null )
+function getPostPanelView($tpl = null, $panelId = null, $postId = null)
 {
-    $model = getPostPanelModel( $panelId, $postId );
-    if (!is_null( $model )) {
-        return new SimpleView( $tpl, $model->export() );
+    $model = getPostPanelModel($panelId, $postId);
+    if (!is_null($model)) {
+        return new SimpleView($tpl, $model->export());
 
     }
 
@@ -278,20 +283,20 @@ function getPostPanelView( $tpl = null, $panelId = null, $postId = null )
  * @param $panelId
  * @return mixed
  */
-function getOptionsPanel( $panelId )
+function getOptionsPanel($panelId)
 {
     /** @var \Kontentblocks\Panels\PanelRegistry $registry */
-    $registry = Kontentblocks()->getService( 'registry.panels' );
-    $panel = $registry->get( $panelId );
-    if (!empty( $panel ) && class_exists( $panel['class'] )) {
-        return new $panel['class']( $panel );
+    $registry = Kontentblocks()->getService('registry.panels');
+    $panel = $registry->get($panelId);
+    if (!empty($panel) && class_exists($panel['class'])) {
+        return new $panel['class']($panel);
     }
 }
 
-function getOptionsPanelModel( $panelId )
+function getOptionsPanelModel($panelId)
 {
-    $panel = getOptionsPanel( $panelId );
-    if (is_a( $panel, '\Kontentblocks\Panels\OptionsPanel' )) {
+    $panel = getOptionsPanel($panelId);
+    if (is_a($panel, '\Kontentblocks\Panels\OptionsPanel')) {
         return $panel->setupFrontendData();
     }
 }
@@ -301,7 +306,7 @@ function getOptionsPanelModel( $panelId )
  */
 function EditScreenLayoutsRegistry()
 {
-    return Kontentblocks()->getService( 'registry.screenLayouts' );
+    return Kontentblocks()->getService('registry.screenLayouts');
 }
 
 /**
@@ -309,5 +314,5 @@ function EditScreenLayoutsRegistry()
  */
 function JSONTransport()
 {
-    return Kontentblocks()->getService( 'utility.jsontransport' );
+    return Kontentblocks()->getService('utility.jsontransport');
 }
