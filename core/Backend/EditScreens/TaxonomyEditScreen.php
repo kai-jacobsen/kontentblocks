4b6784fda3fc3c9d6c67f11fb386a8c403a8477e
Kai Jacobsen <ungestaltbar@gmail.com>
2016-04-02 11:26:01 +0200

    prepare term panels for 4.5

--- core/Backend/EditScreens/TaxonomyEditScreen.php
+++ core/Backend/EditScreens/TaxonomyEditScreen.php
@@ -12,20 +12,24 @@ use Kontentblocks\Utils\Utilities;
 class TaxonomyEditScreen
 {
 
-
     public $taxonomy;
 
+    /**
+     *
+     * TaxonomyEditScreen constructor.
+     */
     public function __construct()
     {
-
-        global $pagenow;
-        if (isset( $_REQUEST['taxonomy'] )) {
-            $this->taxonomy = filter_var( $_REQUEST['taxonomy'], FILTER_SANITIZE_STRING );
-            if (in_array( $pagenow, array( 'edit-tags.php' ) ) && !empty( $this->taxonomy )) {
-                add_action( "{$this->taxonomy}_pre_edit_form", array( $this, 'preparePanels' ), 10, 2 );
-                add_action( "edit_$this->taxonomy", array( $this, 'preparePanels' ), 10, 2 );
+        add_action('current_screen', function ($screen) {
+            if (isset($screen->taxonomy)) {
+                $this->taxonomy = filter_var($screen->taxonomy, FILTER_SANITIZE_STRING);
+                if (in_array($screen->base, array('edit-tags', 'term')) && !empty($this->taxonomy)) {
+                    add_action("{$this->taxonomy}_pre_edit_form", array($this, 'preparePanels'), 10, 2);
+                    add_action("edit_$this->taxonomy", array($this, 'preparePanels'), 10, 2);
+                }
             }
-        }
+
+        });
     }
 
 
@@ -33,10 +37,10 @@ class TaxonomyEditScreen
      * @param $term
      * @param $taxonomy
      */
-    public function preparePanels( $term, $taxonomy )
+    public function preparePanels($term, $taxonomy)
     {
-        $term = get_term( $term );
-        Utilities::getTermEnvironment( $term->term_id, $this->taxonomy );
+        $term = get_term($term);
+        Utilities::getTermEnvironment($term->term_id, $this->taxonomy);
     }
 
 }
