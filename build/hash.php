968142cb683487df4e5349636dfa7047b9221175
Kai Jacobsen <ungestaltbar@gmail.com>
2016-07-05 23:06:46 +0200

    added select2 for template selector one settings.yml for modules

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "3d4ff909e78db673b12420946839f695c1771d3f";
+	return "d524b743adb47f3b0003c4fb137ed96dbf7d66d0";
 };
