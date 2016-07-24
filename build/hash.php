a834427114318a5e03f0079cfa0a01ac33990b5b
Kai Jacobsen <ungestaltbar@gmail.com>
2016-07-24 22:57:53 +0200

    select2 sortable

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "e7e4c36f47b15d36d07938162e88609b9e48c065";
+	return "11603a7e99d16ef4ce3b47a352476f07ab23415e";
 };
