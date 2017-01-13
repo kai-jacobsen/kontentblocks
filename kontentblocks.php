105b18cdc882d4491c4b77fcfde5b1bb1fa6e61f
Kai Jacobsen <ungestaltbar@gmail.com>
2017-01-13 10:06:42 +0100

    testing version update

--- kontentblocks.php
+++ kontentblocks.php
@@ -3,7 +3,7 @@
   Plugin Name: Kontentblocks
   Plugin URI: http://kontentblocks.de
   Description: Content modularization framework
-  Version: 0.8.7
+  Version: 0.8.8
   Author: Kai Jacobsen
   Author URI: https://github.com/kai-jacobsen/kontentblocks
   Text Domain: Kontentblocks
@@ -48,7 +48,7 @@ use Reframe\Autoloader;
 Class Kontentblocks
 {
 
-    const VERSION = '0.8.7';
+    const VERSION = '0.8.8';
     const DEVMODE = true;
     const TABLEVERSION = '1.0.13';
     const DEBUG = false;
