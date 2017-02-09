70144b2655b750f277d3168bbc016b84b2eb2c4a
Kai Jacobsen <ungestaltbar@gmail.com>
2017-02-09 19:45:08 +0100

    rebuilt

--- kontentblocks.php
+++ kontentblocks.php
@@ -3,7 +3,7 @@
   Plugin Name: Kontentblocks
   Plugin URI: https://github.com/kai-jacobsen/kontentblocks
   Description: Content modularization framework
-  Version: 0.8.25
+  Version: 0.8.26
   Author: Kai Jacobsen
   Author URI: https://github.com/kai-jacobsen/kontentblocks-plugin
   Text Domain: Kontentblocks
@@ -47,7 +47,7 @@ use Pimple;
 Class Kontentblocks
 {
 
-    const VERSION = '0.8.25';
+    const VERSION = '0.8.26';
     const DEVMODE = true;
     const TABLEVERSION = '1.0.13';
     const DEBUG = true;
