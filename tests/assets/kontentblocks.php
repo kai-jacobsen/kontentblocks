40987db92b25364c70bfaffa5ee17109eeea27d6
Kai Jacobsen <ungestaltbar@gmail.com>
2017-10-14 09:42:24 +0200

    travis config update

--- tests/assets/kontentblocks.php
+++ tests/assets/kontentblocks.php
@@ -16,9 +16,7 @@
         'name' => 'Demo Page Content', // public shown name
         'description' => 'A single demo area', // public description
         'postTypes' => array( 'post' ), // array of post types where this area is available to
-        'pageTemplates' => array( 'default' ), // array of page template names where this area is available to
         'assignedModules' => array(), // array of classnames
-        'layouts' => array( 'default', '2-columns', '3-columns' ),
         'dynamic' => false, // whether this is an dynamic area
         'manual' => true, // true if set by code
         'limit' => 0, // how many modules are allowed
