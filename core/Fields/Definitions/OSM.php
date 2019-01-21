8bb85527c564f2a3b61701b938e8c1e51f96caaf
Kai Jacobsen <ungestaltbar@gmail.com>
2019-01-21 20:51:58 +0100

    OSM leaflet enqueues

--- core/Fields/Definitions/OSM.php
+++ core/Fields/Definitions/OSM.php
@@ -15,7 +15,7 @@ Class OSM extends Field
     public static function init()
     {
 
-        add_action('admin_enqueue_scripts', function () {
+        add_action('wp_enqueue_scripts', function () {
             wp_register_style('leaflet-css', KB_PLUGIN_URL . 'css/assets/' . 'leaflet_incl_geocoder.css');
             wp_register_script('leaflet-js', KB_PLUGIN_URL . 'js/vendor/leaflet.js');
 //            wp_register_script('leaflet-geocoder', KB_PLUGIN_URL . 'js/vendor/leaflet.js');
