e1ab21a98d8c7591c3aa7eb245e74e939c2122e5
Kai Jacobsen <ungestaltbar@gmail.com>
2019-02-05 12:07:18 +0100

    OSM fix

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "0fd1fa5dc49eda8b5dc150f74b6ce9972d1d68db";
+	return "b10c4892b009d4fc149d749fa687602ebb1312c9";
 };
