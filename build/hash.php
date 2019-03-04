25be4a48e7b58d9f75899083338dc18ed136d22e
Kai Jacobsen <ungestaltbar@gmail.com>
2019-03-04 17:42:56 +0100

    updated rel IDs

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "d30c14e382e997aa213280f9433dcf6ea94fdff7";
+	return "1cc09424b25677edb1160ce45dbab435ad445e72";
 };
