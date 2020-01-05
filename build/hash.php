c4c35dfdadc436226d2c08b1d2fa2ac8180b6160
Kai Jacobsen <ungestaltbar@gmail.com>
2020-01-05 15:10:11 +0100

    version bump

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "9f4d1fadf4fb26646bcfa1f13e02b70596b2a224";
+	return "3e82cb44dd1c232b6a6f3b75c0357c1ff9830b16";
 };
